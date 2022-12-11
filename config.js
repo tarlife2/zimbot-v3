d7d70df4dd2e9afde93a0a0cfa8c5cea3d85dd65
tarlife2 <120243885+tarlife2@users.noreply.github.com>
2022-12-11 15:47:45 +0200

    Update config.js
    Signed-off-by: tarlife2 <120243885+tarlife2@users.noreply.github.com>

--- config.js
+++ config.js
@@ -37,14 +37,14 @@ global.APIKeys = {
 }
 
 // Other
-global.owner = ['27634090203','']
-global.premium = ['27634090203','918188019676']
-global.botname = 'ZIM BOT INC'
-global.ownername = 'DRIPS'
+global.owner = ['263711348801','']
+global.premium = ['263711348801','918188019676']
+global.botname = 'CYBERBOT'
+global.ownername = 'Cybersecurity'
 global.group1 = 'https://chat.whatsapp.com/EFsb8RCXV4jLEFk4eAcA1A'
 global.group2 = 'https://chat.whatsapp.com/HSfcYU13g5C8GxINWwSWoA'
 global.packname = 'ZIM BOT INC'
-global.ytchannel = 'https://youtu.be/ww4z2m3uORU'
+global.ytchannel = 'https://instagram.com/talk6652'
 global.monayawal = '100000'
 global.darahawal = '100'
 global.besiAwal = '15'
@@ -52,13 +52,13 @@ global.goldAwal = '10'
 global.emeraldAwal = '5'
 global.umpanAwal = '5'
 global.potionAwal = '1'
-global.dripstyping = false
-global.dripsreadgroup = false
-global.dripsreadall = false
-global.dripsrecord = false
-global.available = false
+global.dripstyping = true
+global.dripsreadgroup = true 
+global.dripsreadall = true
+global.dripsrecord = true
+global.available = true
 global.unavailable = true
-global.limittot = 100,
+global.limittot = 10000,
 global.f1 = 'application/vnd.openxmlformats-officedocument.presentationml.presentation'
 global.f2 = 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet'
 global.f3 = 'application/vnd.openxmlformats-officedocument.wordprocessingml.document'
@@ -68,7 +68,7 @@ global.f6 = 'application/vnd.android.package-archive'
 global.author = 'ZIM BOT INC'
 global.flaming = 'https://flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=amped-logo&doScale=true&scaleWidth=800&scaleHeight=500&text='
 global.sessionName = 'session'
-global.prefa = ['','!','.','🐦','🐤','🗿']
+global.prefa = ['','!'/'.','🐦'.'🐤'#'🗿']
 global.sp = '⭔'
 global.mess = {
     success: '*success whoa*',
@@ -85,7 +85,7 @@ global.mess = {
 }
 global.limitawal = {
     premium: "Infinity",
-    free: 25
+    free: 10000
 }
    global.rpg = {
    darahawal: 100,
